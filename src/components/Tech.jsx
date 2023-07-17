37ac794d725d3291a7cb0d11104f2acc397dfbba
Chaitya0623 <chaitya0623@gmail.com>
2023-07-17 17:58:06 +0530

    Resolved Projects Error

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,7 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-5'>
+    <div className='flex flex-row flex-wrap justify-center gap-5 hide'>
       {technologies.map((technology) => (
         <div className='w-20 h-20' key={technology.name}>
           <BallCanvas icon={technology.icon} />
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -60,6 +60,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="hide"
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
