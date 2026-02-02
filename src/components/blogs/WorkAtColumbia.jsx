c37fa2f39d84c47194e4d159eee7c976f18b685c
Chaitya0623 <chaitya0623@gmail.com>
2026-02-01 23:33:10 -0500

    Snowflake Blog

---
+++ src/components/blogs/WorkAtColumbia.jsx
@@ -0,0 +1,45 @@
+Considering i got an opportunity to study at an ivy league, it was very important 
+to use this opportunity well by taking the right decision. This calls us for the 
+opportunities present at columbia, and how i made the most out of my time at it.
+
+The MSDS program at columbia university has a set of 7 core courses and 3 electives.
+The good dthing about this program is that it allows you to waive the core courses
+if you show prior experience or good grades for similar topics covered in your previous education
+I took this opportunity to waive 3 courses (3 was the max you can waive) whcih 
+allowed me to take more electives and explore more topics of my interest.
+
+Another important aspect of columbia is its location. Being in new york city,
+it provides a lot of opportunities to network and find internships or jobs.
+I made sure to attend various networking events, career fairs, and workshops
+organized by the university to connect with professionals in my field. So now, it was
+time to use the most out of columbia's location and the education program.
+
+First Semester
+
+IEOR8100E: Agentic AI and Data Economy: rather than going easy on the core courses,
+I decided to take a challenging core course that was relevant to my interests.
+This course provided insights into the intersection of AI and economics. As I had already 
+worked on AI agents at scale, I managed to get in this PHD level course with just 40 people 
+inside and 170+ in the waitlist (cold mailing is all it takes). This course covered 
+the basics of agentics, mcp servers along with presenting a paper and submitting a final project 
+as part of the grade. The good part was that we had lectures from industry experts from
+companies like IBM and Ethereum which explained the potential use cases and how we could use 
+Ai agents to build decentralized applications. I worked on a project on auto-refining smart contract generator.
+The details of this project can be found in my projects section (https://github.com/AgenticsFintekColumbia/smart-contracts).
+At the end of this course, i got an opportunity to contribute on IBM's Agentics repo
+and work on their internal structure. My project, along with a few Phd student projects were selected 
+to be published on columbia's official website. Sir also told that my project will be
+used as a potential study material for the course for the upcoming semsesters. This led
+to me getting to work as a research assistant for prof. Agostino Capponi, the course instructor for the upcoming semester.
+
+COMS6998E: High Performance Machine Learning
+
+STAT5702GR: Exploratory Data Analysis & Visualisation
+
+COMS4995W: Applied Deep Learning
+
+COMS4995: Natural Language Processing (Teaching Assistant)
+
+Second Semester
+
+Stay Tuned for for further updates for the upcoming semester
